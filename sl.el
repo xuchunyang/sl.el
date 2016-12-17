51d92f820f3e93776fff6cdb9690458816888bdc
Chunyang Xu <mail@xuchunyang.me>
2016-12-17 21:55:27 +0800

    Bump v0.2

--- sl.el
+++ sl.el
@@ -5,7 +5,7 @@
 ;; Author: Chunyang Xu <mail@xuchunyang.me>
 ;; URL: https://github.com/xuchunyang/sl.el
 ;; Package-Requires: ((cl-lib "0.5"))
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
